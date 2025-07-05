03a2d8c7c54f303f44a4ae1738b81bf6349b1dee
EES_NITWarangal <kauravpradeep542@gmail.com>
2025-07-05 23:33:05 +0530

    updated database environment

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Prodeep#sql2255', // Set your MySQL root password here if you have one
-  database: 'wastenot',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'Prodeep#sql2255',
+  database: process.env.DB_NAME || 'wastenot',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
