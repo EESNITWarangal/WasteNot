72b85c4c2f5f52e7f6aa5ff5286a8a727f6f79b1
EESNITWarangal <kauravpradeep542@gmail.com>
2025-07-05 22:10:15 +0530

    backend serving frontend

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const foodRoutes = require('./routes/food');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Food Redistribution Platform Backend');
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
